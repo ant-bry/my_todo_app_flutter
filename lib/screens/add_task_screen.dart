55c6b5b6d4ae17297fa0de1e2b7376258143d67f
anthonybryanm.pagarigan <anthonypagarigan1105@gmail.com>
2020-07-21 12:51:36 +0800

    added some validation

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:my_todo_app/models/task_data.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AddTaskScreen extends StatelessWidget {
   @override
@@ -25,19 +26,33 @@ class AddTaskScreen extends StatelessWidget {
               'Add Task',
               textAlign: TextAlign.center,
               style: TextStyle(
-                color: Colors.lightBlueAccent,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
                 fontSize: 30.0,
               ),
+            ),
+            SizedBox(
+              height: 10.0,
             ),
             TextField(
               autofocus: true,
               textAlign: TextAlign.center,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                helperText: 'Required',
+                border: OutlineInputBorder(),
+              ),
               onChanged: (newText) {
                 newTaskTitle = newText;
+                print(newTaskTitle);
+                if (newText == "") {
+                  print("user typed and removed it");
+                  newTaskTitle = null;
+                }
               },
             ),
             FlatButton(
-              color: Colors.lightBlueAccent,
+              color: Colors.black,
               child: Text(
                 'Add',
                 style: TextStyle(
@@ -50,9 +65,47 @@ class AddTaskScreen extends StatelessWidget {
                     Provider.of<TaskData>(context, listen: false)
                         .addTask(newTaskTitle);
                     Navigator.pop(context);
+                    Fluttertoast.showToast(
+                      msg: "$newTaskTitle added to the list",
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      backgroundColor: Colors.lightBlueAccent,
+                      fontSize: 15.0,
+                    );
                   } else {
                     print('Textfield must not be empty');
-                    Navigator.pop(context);
+
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: Text(
+                          'Textfield must not be empty',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text(
+                              'Ok',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                        backgroundColor: Colors.lightBlueAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                      barrierDismissible: false,
+                    );
                   }
                 } catch (e) {
                   print(e);
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -11,15 +11,66 @@ class TasksList extends StatelessWidget {
         return ListView.builder(
           itemBuilder: (context, index) {
             final task = taskData.tasks[index];
-            return TaskTile(
-              taskTitle: task.name,
-              isChecked: task.isDone,
-              checkboxCallback: (checkboxState) {
-                taskData.updateTask(task);
-              },
-              longPressCallback: () {
-                taskData.deleteTask(task);
-              },
+            return Card(
+              elevation: 1,
+              child: TaskTile(
+                taskTitle: task.name,
+                isChecked: task.isDone,
+                checkboxCallback: (checkboxState) {
+                  taskData.updateTask(task);
+                },
+                longPressCallback: () {
+                  print('Long pressed!');
+                  try {
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: Text(
+                          'Delete this task?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text(
+                              'Yes',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {
+                              taskData.deleteTask(task);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text(
+                              'No',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                      barrierDismissible: false,
+                    );
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
             );
           },
           itemCount: taskData.taskCount,
